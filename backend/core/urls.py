ab6e92498c4182507a068c18b30fd62656fe6b0c
Aniket Kadukar <aniketdkadukar@gmail.com>
2023-08-27 23:29:10 +0530

    Initial Commit

---
+++ backend/core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('blog.urls', 'blog'), namespace='blog')),
+    path('api/', include(('blog_api.urls', 'blog_api'), namespace='blog_api')),
+]
